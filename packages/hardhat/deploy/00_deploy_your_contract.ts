60d578a507113b5925f4a38c6fbfb65680157c9b
NightmareFox <carloshenriquez392004@gmail.com>
2025-06-11 10:38:05 -0400

    Update contract deployment arguments to use USDC contract address instead of deployer address

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -25,7 +25,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   await deploy("YourContract", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"], //usdc contract
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
