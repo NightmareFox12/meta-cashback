30571f26aa74294710b68f6cbb709558865395ae
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-06-17 23:05:48 -0400

    Add ScrollArea component and enhance DialogStake with route display and loading state

--- packages/nextjs/app/staking/_components/DialogStake.tsx
+++ packages/nextjs/app/staking/_components/DialogStake.tsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { Route } from "@lifi/sdk";
 import { LoaderCircle } from "lucide-react";
 import { NextPage } from "next";
+import { Badge } from "~~/components/shad/ui/badge";
 import { Button } from "~~/components/shad/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~~/components/shad/ui/card";
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "~~/components/shad/ui/dialog";
+import { ScrollArea } from "~~/components/shad/ui/scroll-area";
 
 type DialogStakeProps = {
   chainID: number | undefined;
@@ -19,9 +22,11 @@ const DialogStake: NextPage<DialogStakeProps> = ({ chainID, loadRoutes, handleBr
     const res = await handleBrigeUSDC();
 
     console.log(res);
+    setRoutes(res);
   };
+
   return (
-    <DialogContent>
+    <DialogContent className="bg-base-200">
       <DialogHeader>
         <DialogTitle>Are you absolutely sure?</DialogTitle>
         <DialogDescription>
@@ -32,16 +37,48 @@ const DialogStake: NextPage<DialogStakeProps> = ({ chainID, loadRoutes, handleBr
                 <span className="text-red-400"> Optimism</span> securely and quickly using LI.FI technology.
               </p>
               <div className="w-full flex justify-center my-2">
-                <Button onClick={handleSearchRoutes} disabled={loadRoutes}>
-                  {loadRoutes ? (
-                    <>
-                      <LoaderCircle className="animate-spin" />
-                      Searching...
-                    </>
-                  ) : (
-                    "Search routes"
-                  )}
-                </Button>
+                {routes && routes?.length > 0 ? (
+                  <ScrollArea className="h-[250px] w-full p-2">
+                    {routes.map((v, k) => (
+                      <Card className="m-2 bg-primary rounded-sm" key={k}>
+                        <CardHeader>
+                          <CardTitle>
+                            {v.fromChainId}
+                            {v.toChainId}
+                          </CardTitle>
+                        </CardHeader>
+                        <CardContent></CardContent>
+
+                        <CardFooter className="flex justify-between items-center">
+                          <p className="text-[12px]">
+                            Gast Cost: <span className=" font-semibold">{v.gasCostUSD}</span>
+                          </p>
+
+                          {v.tags && v.tags.length > 0 && (
+                            <div className="flex gap-1">
+                              {v.tags.map((tag, key) => (
+                                <Badge key={key} variant="secondary" className="text-[10px]">
+                                  {tag}
+                                </Badge>
+                              ))}
+                            </div>
+                          )}
+                        </CardFooter>
+                      </Card>
+                    ))}
+                  </ScrollArea>
+                ) : (
+                  <Button onClick={handleSearchRoutes} disabled={loadRoutes}>
+                    {loadRoutes ? (
+                      <>
+                        <LoaderCircle className="animate-spin" />
+                        Searching...
+                      </>
+                    ) : (
+                      "Search routes"
+                    )}
+                  </Button>
+                )}
               </div>
             </>
           ) : (
